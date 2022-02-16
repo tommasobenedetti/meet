2c485bc9e917026e0bd863a28b67b3f59aa20830
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-02-16 20:17:31 +0100

    still fixing

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://tommasobenedetti.github.io/meet/"],
+  redirect_uris: ["https://tommasobenedetti.github.io/meet/", "http://localhost:8080/test-auth-server.html"],
   javascript_origins: ["https://tommasobenedetti.github.io", "http://localhost:3000", "http://localhost:8080"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
@@ -89,7 +89,7 @@ module.exports.getAccessToken = async (event) => {
         return {
           statusCode: 200,
           headers: {
-            "Access-Control-Allow-Origin": "*"
+            'Access-Control-Allow-Origin': '*'
           },
           body: JSON.stringify(token),
         };
