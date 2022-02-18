b79d4b9e07d6b1a55c40b780e464e1f12bd5dcbb
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-02-18 17:00:03 +0100

    Revert "Update handler.js"
    This reverts commit bbff150cc3804c7066ff8b19c1df655c5835ff57.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -52,9 +52,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods:": "*",
-      "Access-Control-Allow-Credentials": true
+      "Access-Control-Allow-Origin": "*"
     },
     body: JSON.stringify({
       authUrl: authUrl,
@@ -91,9 +89,7 @@ module.exports.getAccessToken = async (event) => {
         return {
           statusCode: 200,
           headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods:": "*",
-            "Access-Control-Allow-Credentials": true
+            'Access-Control-Allow-Origin': '*'
           },
           body: JSON.stringify(token),
         };
@@ -145,9 +141,7 @@ module.exports.getCalendarEvents = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods:": "*",
-          "Access-Control-Allow-Credentials": true
+          "Access-Control-Allow-Origin": "*"
         },
         body: JSON.stringify({ events: results.data.items }),
       };
