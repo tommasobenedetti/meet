7492321506d30dd142b6b74e2ec394de9809ccbe
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-03-11 17:44:15 +0100

    fix scenario 1

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -17,7 +17,7 @@ defineFeature(feature, test => {
             AppWrapper = mount(<App />);
         });
 
-        then('the user should see the list of upcoming events from all locations', () => {
+        then('the user should see the list of upcoming events.', () => {
             AppWrapper.update();
             expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
         });
