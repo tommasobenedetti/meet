ef1749937b88f1afd5ea5d65a51a3ea6bb68c06c
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-03-15 12:35:15 +0100

    Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('8401334477d54855b85cc2a5e86518d9').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
