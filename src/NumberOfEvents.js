8047b058dc4928ca67304717432dfa4a6391c6c5
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-04-05 18:41:25 +0200

    Revert "Update NumberOfEvents.js"
    This reverts commit 25dfc1cae8b5a8170f59ecc74f8a8f8f8e809910.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,7 +3,7 @@ import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
     state = {
-        numberOfEvents: '',
+        numberOfEvents: '32',
         infoText: ""
     }
 
